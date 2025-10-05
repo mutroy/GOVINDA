2e0a86c4db5ce9554d0355cf16b358f98e000b87
MU <mericajam@gmail.com>
2025-10-04 22:15:14 -0400

    Commit#9 - Chapter 3 done and Chapter 4 just created the file

--- src/Sheryians_Module1/Chapter3_Coding_Conventions.java
+++ src/Sheryians_Module1/Chapter3_Coding_Conventions.java
@@ -1,4 +1,20 @@
 package Sheryians_Module1;
 
 public class Chapter3_Coding_Conventions {
+    public static void main(String[] args) {
+
+        // Naming convention allowed
+        int $a = 23;
+        int _b = 32;
+        int c9 = 232;
+
+        // Case sensitive
+
+        int a;
+        int A;
+
+
+
+
+    }
 }
---
+++ src/Sheryians_Module1/Chapter4_Data_types.java
@@ -0,0 +1,7 @@
+package Sheryians_Module1;
+
+public class Chapter4_Data_types {
+    public static void main(String[] args) {
+
+    }
+}
