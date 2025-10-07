89ee71981f794927b47c56f8e75c193155044b72
MU <mericajam@gmail.com>
2025-10-07 16:52:56 -0400

    Commit#10 Chapter 4 Notes

--- src/Sheryians_Module1/Chapter4_Data_types.java
+++ src/Sheryians_Module1/Chapter4_Data_types.java
@@ -3,5 +3,26 @@ package Sheryians_Module1;
 public class Chapter4_Data_types {
     public static void main(String[] args) {
 
+        byte b = 12;
+        short s = 2;
+        int i;
+        long li;
+        float fl = 5.6f;
+        double dl;
+        char ch;
+        boolean bl;
+
+        // This is error over here int a = 2147483648L;
+
+        System.out.println("Data types: ");
+
+        char cha = 'a';
+        System.out.println(cha);
+
+        for(char in =0;in<65535;in++){
+            System.out.print(in);
+            if(in%100 == 0) System.out.println();
+        }
+
     }
 }
---
+++ src/Sheryians_Module1/Chapter5_Scanner_API.java
@@ -0,0 +1,7 @@
+package Sheryians_Module1;
+
+public class Chapter5_Scanner_API {
+    public static void main(String[] args) {
+        
+    }
+}
