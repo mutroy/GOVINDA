cd922820cdb58582cc0ae62549680964bef78fe2
MU <mericajam@gmail.com>
2025-10-07 21:46:50 -0400

    Commit#12 - Chapter 6 Completed

---
+++ src/Sheryians_Module1/Chapter6_Operators.java
@@ -0,0 +1,17 @@
+package Sheryians_Module1;
+
+public class Chapter6_Operators {
+    public static void main(String[] args) {
+
+        // Unary + -
+        // Binary + - / *
+        // Arithmetic  + - / * % ++ --
+
+        int a=7, b = 3;
+        System.out.println(a/b);
+        System.out.println(a%b);
+
+        double db = a/b; // int / int will not produce the decimal value
+        System.out.println(db);
+    }
+}
---
+++ src/Sheryians_Module1/Chapter6_Operators_Char_operation.java
@@ -0,0 +1,9 @@
+package Sheryians_Module1;
+
+public class Chapter6_Operators_Char_operation {
+    public static void main(String[] args) {
+
+        char ch = 'a';
+        System.out.println(ch++ + ch); // Converted to int and their ascii values are added
+    }
+}
