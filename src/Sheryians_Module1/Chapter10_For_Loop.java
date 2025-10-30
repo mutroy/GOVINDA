bc9dec615b2465562806046f39967854374d4e51
MU <mericajam@gmail.com>
2025-10-30 14:07:24 -0400

    Commit#17 Chapter 10 Files

---
+++ src/Sheryians_Module1/Chapter10_For_Loop.java
@@ -0,0 +1,15 @@
+package Sheryians_Module1;
+
+public class Chapter10_For_Loop {
+    public static void main(String[] args) {
+
+        int i;
+//        for(i =1; i<=10 ; i++){
+//            System.out.println(i);
+//        }
+
+        for(i = 1; i<= 5; System.out.println(i++));
+        System.out.println("after loop " + i);
+
+    }
+}
